d226ede0e3564239dbd9d53c8d76b97a4d78cb04
Edlly Gómez <egomez@saatvamattress.com>
2022-04-06 13:20:25 -0600

    First set of changes

---
+++ tests/test.js
@@ -0,0 +1,27 @@
+const webdriverio = require('webdriverio');
+const assert = require('chai').assert;
+
+const iosOptions = {
+    path: '/wd/hub',
+    host: 'localhost',
+    port: 4723,
+    logLevel: 'info',
+    capabilities: { 
+        platformName: 'iOS',
+        automationName: 'XCUITest',
+        deviceName: 'iPhone Simulator',
+        platformVersion: '13',
+        browserName: 'Safari'
+    }
+}
+
+
+describe('Create Safari session', function () {
+  it('should create and destroy IOS Safari session', async function () {
+    const client = await webdriverio.remote(iosOptions);
+    await client.url('https://www.google.com');
+    const title = await client.getTitle();
+    assert.equal(title, 'Google');
+    await client.deleteSession();
+  });
+});
